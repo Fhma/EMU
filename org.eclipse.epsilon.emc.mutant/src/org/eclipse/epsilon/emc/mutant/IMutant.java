e43d9fc42f1a5954a4962ab5446653cdd96f9810
fhma <alhwikem@gmail.com>
2021-09-22 18:44:53 +0300

    clean up for 1.8

--- org.eclipse.epsilon.emc.mutant/src/org/eclipse/epsilon/emc/mutant/IMutant.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.epsilon.emc.mutant;
-
-import org.eclipse.epsilon.eol.models.IModel;
-import org.eclipse.epsilon.emc.mutant.IProperty;;
-
-public interface IMutant extends IModel {
-	public IProperty getProperty(Object object, String p_name);
-}
